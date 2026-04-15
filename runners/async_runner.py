d9598133434b35fa198298cfe181451c644c180d
hchothani <harshdchothani@gmail.com>
2026-04-15 13:24:47 +0530

    testing ray for async

--- runners/async_runner.py
+++ runners/async_runner.py
@@ -16,7 +16,7 @@ from clients.async_client import create_simulated_clients
 @ray.remote
 def execute_ray_client(client_idx: int, client, params, start_timestamp) -> tuple:
     from flwr.common import FitIns
-    config = {"start_timestamp": start_time_stamp}
+    config = {"start_timestamp": start_timestamp}
     return client_idx, client.fit(FitIns(parameters=params, config=config))
 
 def get_async_config(cfg: DictConfig) -> Dict[str, Any]:
