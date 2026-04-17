701aa236a32db261cbe5db6aea65a2cb545940cc
hchothani <harshdchothani@gmail.com>
2026-04-17 20:35:18 +0530

    fixed wb name

--- utils/logger.py
+++ utils/logger.py
@@ -25,7 +25,7 @@ def generate_run_name(cfg: DictConfig, is_async: bool) -> str:
             continue
             
         # Format Boolean flags beautifully (e.g., delay: True -> "delay", False -> "nodelay")
-        if isinstance(val, bool):
+        if val in ["true", "false"]:
             flag_name = key.split('.')[-1] # Grabs just the word 'delay'
             parts.append(flag_name if val else f"no_{flag_name}")
             
